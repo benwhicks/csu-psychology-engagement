ab51e6532ea0f0f4406c99fafdf7dbfb1df06440
Ben Hicks <benwhicks@gmail.com>
2019-07-26 23:18:05 +1000

    triadic network analysis

--- R/cluster-1-2-socpsych-act-graphs.R
+++ R/cluster-1-2-socpsych-act-graphs.R
@@ -21,89 +21,116 @@ library(ggsci)
 library(data.table)
 
 source(file.path( 'R', 'psych_functions.R'))
-source(file.path( 'R', 'import-data.R'))
+
+library(rdata.psych)
+ZERODATE <- '2018-06-04'
+SESSIONSTART <- '2018-07-09'
+SESSIONEND <- '2018-10-26'
 
 # Graphing ----------------------------------------------------------------
 # Building underlying site structure
-cc_nodes <-  nodes_from_cc(ccSoc18, subject = "Soc")
-cc_edges <-  edges_from_cc(ccSoc18)
+content_primary_keys <- sort(unique(ccSoc18$pk1))
+content_primary_keys <- factor(content_primary_keys, levels = content_primary_keys)
+content_levels <- levels(content_primary_keys)
+
+cc_nodes <-  ccSoc18 %>% 
+  select(id = pk1, title, data, handle) %>% 
+  mutate(id = factor(id, levels = content_levels))
+
+cc_edges <-  tibble(
+  from = factor(ccSoc18$parent_pk1, levels = content_levels),
+  to = factor(ccSoc18$pk1, levels = content_levels),
+  alfalfa = 1.0
+) %>% 
+  filter(!is.na(from), !is.na(to))
+
 cc_edges <- bind_rows(cc_edges %>% mutate(cluster = 1), 
-                      cc_edges %>% mutate(cluster = 2)) # might need to add time variable for faceting in a similar repeated manner
+                      cc_edges %>% mutate(cluster = 2), 
+                      cc_edges %>% mutate(cluster = 3), 
+                      cc_edges %>% mutate(cluster = 4), 
+                      cc_edges %>% mutate(cluster = 5)) 
+cc_edges <- bind_rows(cc_edges %>% mutate(time = as_datetime('2018-07-01')),
+                      cc_edges %>% mutate(time = as_datetime('2018-08-01')),
+                      cc_edges %>% mutate(time = as_datetime('2018-09-01')),
+                      cc_edges %>% mutate(time = as_datetime('2018-10-01')))
 cc_graph <- tbl_graph(nodes = cc_nodes, edges = cc_edges)
 cc_layout <- create_layout(cc_graph, layout = "nicely")
 np <- data.frame(x = cc_layout$x, y = cc_layout$y)
 
 # Student activity overlay
-aaSocCluster12 <- aaSoc18 %>% 
-  filter(cluster %in% 1:2, timestamp < as.Date('2018-10-30'))
+aaEdgePrep <- aaSoc18 %>% 
+  data.table() %>% 
+  .[timestamp <= as.Date('2018-10-30') & timestamp > as.Date('2018-07-02') , 
+    .(timestamp, session, person, cluster, content_pk1)] %>% 
+  setorder(person, timestamp)
+
+i_to <- 2:nrow(aaEdgePrep)
+i_fm <- 1:(nrow(aaEdgePrep) - 1)
+
+aa_edges <- data.table(
+  from =   factor(aaEdgePrep$content_pk1[i_fm], levels = content_levels),
+  to =     factor(aaEdgePrep$content_pk1[i_to], levels = content_levels),
+  pfrom =  aaEdgePrep$person[i_fm],
+  pto =    aaEdgePrep$person[i_to],
+  sfrom =  aaEdgePrep$session[i_fm],
+  sto =    aaEdgePrep$session[i_to],
+  time =   aaEdgePrep$timestamp[i_fm],
+  cluster = aaEdgePrep$cluster[i_fm],
+  alfalfa = 0.05
+) %>% .[pfrom == pto & sfrom == sto, .(from, to, time, cluster, alfalfa)] %>% 
+  .[!is.na(from) & !is.na(to)]
+
 
 # Single cluster edge compression
-cluster_edge_compression <- function(x) {
-  if (length(unique(x$cluster)) != 1) stop("Number of unique clusters not 1")
-  
+edge_compression <- function(x) {
+  x[!is.na(time) , time := floor_date(time, unit = "month")]
+  x <- x %>% 
+    group_by(from, to, time, cluster, alfalfa) %>% 
+    summarise(weight = log(n() + 1))
+  return(x)
 }
 
-# Cluster summary fn - using cluster as pk instead of student
-cluster_aa_compression <- function(aa) {
-  aa %>% 
-    filter(role == 'S', !is.na(content_pk1), timestamp < as.POSIXct('2018-10-30')) %>% 
-    select(id, time = timestamp, content_pk1, session) %>%  
-    #grade_quartile, cluster, qual) %>% 
-    mutate(id = factor(id), content_pk1 = factor(content_pk1)) %>% 
-    arrange(id, time)
-}
-
-aa_edges <-   
-  x %>% 
-  filter(role == 'S', !is.na(content_pk1), timestamp < as.POSIXct('2018-10-30')) %>% 
-  select(id, time = timestamp, content_pk1, session) %>%  
-  #grade_quartile, cluster, qual) %>% 
-  mutate(id = factor(id), content_pk1 = factor(content_pk1)) %>% 
-  arrange(id, time)
-aa_edges <- tibble(from = aa$content_pk1[1:(nrow(aa) - 1)],
-                   to =   aa$content_pk1[2:nrow(aa)],
-                   time = aa$time[2:nrow(aa)],
-                   student_from = aa$id[1:(nrow(aa) - 1)],
-                   student = aa$id[2:nrow(aa)],
-                   session_from = aa$session[1:(nrow(aa) - 1)],
-                   session = aa$session[2:nrow(aa)])
-#grade_quartile = aa$grade_quartile[1:(nrow(aa) - 1)],
-#cluster = aa$cluster[1:(nrow(aa) - 1)],
-#qual = aa$qual[1:(nrow(aa) - 1)]
-aa_edges <- aa_edges %>% 
-  filter(student == student_from, session == session_from) %>% 
-  select(from, to, time, student, cluster) %>% # , grade_quartile, cluster, qual 
-  mutate(weight = 2)
-return(aa_edges)
-
-
-aa_nodes <-  nodes_from_aa(aaSoc18 
-                         %>% filter(cluster %in% c(1, 2)))
-aa_edges <- edges_from_aa_cluster(aaSoc18 
-                                 %>% filter(cluster %in% c(1, 2)))
+aa_edges <- edge_compression(aa_edges)
 
 aa_graph <- tbl_graph(nodes = cc_nodes, edges = aa_edges)
-joined_graph <- cc_graph %>% graph_join(aa_graph) 
-joined_graph <- joined_graph %>% 
+
+graph <- graph_join(cc_graph, aa_graph)
+
+g1 <- graph %>%
   activate(edges) %>% 
-  mutate(student = if_else(is.na(student), "_course structure", as.character(student)))
-graph_list <- list(joined_graph, np)
+  filter(cluster == 1) %>% 
+  ggraph(layout = "manual", node.positions = np) +
+  geom_node_point() +
+  geom_edge_link(aes(color = as.numeric(time),
+                     group = time,
+                     alpha = alfalfa)) +
+  scale_edge_color_gradientn(colours = c("#bbbbbb", "#666666", "#000000")) +
+  theme_graph()+
+  theme(legend.position = "none",
+        text = element_text(family = "TT Times New Roman")) 
 
-draw_site_activity_graph_greyscale <- function(graph_list, alpha_low = 0.1) {
-  g <- ggraph(graph_list[[1]], layout = "manual", node.positions = graph_list[[2]]) +
-    geom_node_point(color = "#bbbbbb") + 
-    geom_edge_link0(aes(color = as.numeric(time), 
-                        width = weight, 
-                        alpha = 2 - weight,
-                        group = time)) +
-    scale_edge_alpha_continuous(range = c(alpha_low, 1)) +
-    scale_edge_width(range = c(0.6,1)) +
-    scale_edge_color_gradientn(colors = c("#bbbbbb", "#666666", "#000000")) +
-    theme_graph() +
-    theme(legend.position = "none",
-          text = element_text(family = "TT Times New Roman"))
-  return(g)
-}
+g <- graph %>% 
+  ggraph(layout = "manual", node.positions = np) +
+  geom_node_point(size = 0.2) +
+  geom_edge_link(aes(color = alfalfa,
+                     alpha = alfalfa)) +
+  scale_edge_color_gradientn(colours = c("#000000", "#666666", "#888888")) +
+  theme_graph() +
+  theme(legend.position = "none",
+        text = element_text(family = "TT Times New Roman")) +
+  facet_grid(cluster ~ time)
 
-draw_site_activity_graph_greyscale(
-  graph_list = graph_list, alpha_low = 0.02) + facet_wrap(~ cluster) + ggtitle("Social Psychology activity by cluster")
+g12 <- graph %>%
+  activate(edges) %>% 
+  filter(cluster %in% 1:2) %>% 
+  ggraph(layout = "manual", node.positions = np) +
+  geom_node_point(aes(color = handle), size = 0.2) +
+  geom_edge_link(aes(color = alfalfa,
+                     alpha = alfalfa)) +
+  scale_edge_color_gradientn(colours = c("#000000", "#666666", "#888888")) +
+  theme_graph() +
+  theme(legend.position = "none",
+        text = element_text(family = "TT Times New Roman")) +
+  facet_grid(cluster ~ time)
+
+
---
+++ R/plots.R
@@ -0,0 +1,66 @@
+# Various plots
+
+library(rdata.psych)
+library(ggraph)
+library(tidygraph)
+library(ggplot2)
+
+forum_plot <- function(g, iso = T, teacher = T) {
+  if (iso) {
+    data <- g
+  } else {
+    data <- g %>% 
+      activate(nodes) %>% 
+      filter(!node_is_isolated())
+  }
+  if (!teacher) {
+    data <- data %>% 
+      activate(nodes) %>% 
+      filter(str_detect(Person, "Student"))
+  }
+  plot <- data %>% 
+    ggraph(layout = "nicely") +
+    geom_node_point(aes(color = Role, size = Role)) +
+    geom_edge_link(aes(color = weight), alpha = 0.2) +
+    theme_graph() + 
+    theme(legend.position = "none",
+          text = element_text(family = "TT Times New Roman"))
+  return(plot)
+}
+
+forum_plot(forumGraphBio17, iso = F, teacher = T)
+forum_plot(forumGraphBio17, iso = F, teacher = F)
+forum_plot(forumGraphBio18, iso = F, teacher = T)
+
+# Graph analysis
+analyse_graph <- function(g) {
+  n <- length(V(g)) # number of nodes
+  d <- edge_density(g)
+  t <- transitivity(g)
+  apl <- average.path.length(g)
+  # triad census and comparison see - https://www.markanthonyhoffman.com/social_network_analysis/measuring-transitivity-and-structural-balance.html
+  # Academic paper - https://arxiv.org/abs/1502.07016 
+  Mutual_Asymmetric_Null_classes <-   c("003",  "012",  "102",  "021D",
+                     "021U", "021C", "111D", "111U",
+                     "030T", "030C", "201",  "120D",
+                     "120U", "120C", "210",  "300")
+  triads <- triad_census(g)
+  trials <- 500
+  trial_triads <- vector("list", length = trials)
+  trial_transitivity <- c()
+  for (i in 1:trials) {
+    rg <- erdos.renyi.game(n, d, directed = T)
+    trial_triads[[i]] <- triad_census(rg)
+    trial_transitivity[[i]] <- transitivity(rg)
+  }
+  expected_triads <- Reduce("+", trial_triads) / trials
+  expected_transitivity <- mean(trial_transitivity)
+  triad_df <- tibble(MAN_class = Mutual_Asymmetric_Null_classes,
+                     Triads = triads,
+                     expected = expected_triads)
+  return(list(nodes = n, desnsity = d, transitivity = t, expected_transitivity = expected_transitivity,
+              triad_df = triad_df))
+}
+
+# Splitting forums
+unique(fmSoc17$forum)
